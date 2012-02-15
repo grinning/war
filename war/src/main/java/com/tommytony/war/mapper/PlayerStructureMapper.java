dc91793caff4a4c9d49b745722cd923be75c2914
grinning <i.like.banana.ewing@gmail.com>
2012-02-14 23:23:17 -0500

    Started the PlayerMapper Object
    This object will provide an interface of the common player to the files.

---
+++ war/src/main/java/com/tommytony/war/mapper/PlayerStructureMapper.java
@@ -0,0 +1,12 @@
+package com.tommytony.war.mapper;
+
+public class PlayerStructureMapper {
+
+	private String name;
+	
+	public PlayerStructureMapper(String name) {
+		this.name = name;
+	}
+	
+	
+}
