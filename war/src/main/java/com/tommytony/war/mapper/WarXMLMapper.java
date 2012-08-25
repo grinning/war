ded407c4aac9ae547110645f52ef90a068266f1f
grinning <grin@tommytony.com>
2012-08-25 15:47:27 -0400

    More XMLMapper

--- war/src/main/java/com/tommytony/war/mapper/WarXMLMapper.java
+++ war/src/main/java/com/tommytony/war/mapper/WarXMLMapper.java
@@ -15,8 +15,11 @@ import javolution.xml.XMLObjectWriter;
 import javolution.xml.stream.XMLStreamException;
 
 import com.tommytony.war.War;
+import com.tommytony.war.Warzone;
 import com.tommytony.war.config.WarConfig;
 import com.tommytony.war.config.WarConfigBag;
+import com.tommytony.war.config.WarzoneConfig;
+import com.tommytony.war.config.WarzoneConfigBag;
 
 public class WarXMLMapper {
 
@@ -26,6 +29,7 @@ public class WarXMLMapper {
 		binding.setAlias(Boolean.class, "Boolean");
 		binding.setAlias(WarConfig.class, "WarConfig");
 		binding.setAlias(Integer.class, "Integer");
+		binding.setAlias(WarzoneConfig.class, "WarzoneConfig");
 		OutputStream output = null;
 		try {
 			output = new FileOutputStream(War.war.getDataFolder().getPath() + "config.xml");
@@ -36,22 +40,52 @@ public class WarXMLMapper {
 		try {
 			writer = new XMLObjectWriter().setOutput(output).setBinding(binding);
 		} catch (XMLStreamException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		(new File(War.war.getDataFolder().getPath())).mkdir();
 		(new File(War.war.getDataFolder().getPath() + "/dat")).mkdir();
 		List list = new ArrayList();
-		Map<WarConfig, Object> war_config = new FastMap<WarConfig, Object>();
+		Map<WarConfig, Object> war_config = new FastMap<WarConfig, Object>(6);
 		WarConfigBag war_config_bag = War.war.getWarConfig();
-		war_config.put(WarConfig.BUILDINZONESONLY, war_config_bag.getBoolean(WarConfig.BUILDINZONESONLY));
-		war_config.put(WarConfig.DISABLEBUILDMESSAGE, war_config_bag.getBoolean(WarConfig.DISABLEBUILDMESSAGE));
-		war_config.put(WarConfig.DISABLEPVPMESSAGE, war_config_bag.getBoolean(WarConfig.DISABLEPVPMESSAGE));
-		war_config.put(WarConfig.MAXZONES, war_config_bag.getInt(WarConfig.MAXZONES));
-		war_config.put(WarConfig.PVPINZONESONLY, war_config_bag.getBoolean(WarConfig.PVPINZONESONLY));
-		war_config.put(WarConfig.TNTINZONESONLY, war_config_bag.getBoolean(WarConfig.TNTINZONESONLY));
+		war_config.put(WarConfig.BUILDINZONESONLY, false);
+		war_config.put(WarConfig.DISABLEBUILDMESSAGE, false);
+		war_config.put(WarConfig.DISABLEPVPMESSAGE, false);
+		war_config.put(WarConfig.MAXZONES, 12);
+		war_config.put(WarConfig.PVPINZONESONLY, false);
+		war_config.put(WarConfig.TNTINZONESONLY, false);
 		list.add(war_config);
-		
+		Map<WarzoneConfig, Object> warzone_default_config = new FastMap<WarzoneConfig, Object>();
+		WarzoneConfigBag warzone_bag = War.war.getWarzoneDefaultConfig();
+		warzone_default_config.put(WarzoneConfig.AUTOASSIGN, false);
+		warzone_default_config.put(WarzoneConfig.BLOCKHEADS, true);
+		warzone_default_config.put(WarzoneConfig.DEATHMESSAGES, false);
+		warzone_default_config.put(WarzoneConfig.DISABLED, false);
+		warzone_default_config.put(WarzoneConfig.DOMENABLED, false);
+		warzone_default_config.put(WarzoneConfig.DOMTIME, 15);
+		warzone_default_config.put(WarzoneConfig.EASYCONFIG, false);
+		warzone_default_config.put(WarzoneConfig.FRIENDLYFIRE, false);
+		warzone_default_config.put(WarzoneConfig.GLASSWALLS, true);
+		warzone_default_config.put(WarzoneConfig.HEALERS, false);
+		warzone_default_config.put(WarzoneConfig.INFECTION, false);
+		warzone_default_config.put(WarzoneConfig.INSTABREAK, false);
+		warzone_default_config.put(WarzoneConfig.MAXTNT, 5);
+		warzone_default_config.put(WarzoneConfig.MINPLAYERS, 1);
+		warzone_default_config.put(WarzoneConfig.MINTEAMS, 1);
+		warzone_default_config.put(WarzoneConfig.MONUMENTHEAL, 5);
+		warzone_default_config.put(WarzoneConfig.NOCREATURES, false);
+		warzone_default_config.put(WarzoneConfig.NODROPS, false);
+		warzone_default_config.put(WarzoneConfig.PREPTIME, 0);
+		try {
+			writer.write(list, "config");
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		} finally {
+		try {
+			writer.close();
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		}
+		}
 		
 	}
 }
