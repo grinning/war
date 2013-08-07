da9e1b4d0aa2a409d6b4953ff3695595ccb5d6bb
grinning <grin@tommytony.com>
2013-08-07 16:56:01 -0600

    1D array -> 3D array transform

--- war/src/main/java/com/tommytony/war/mapper/StructureVolumeMapper.java
+++ war/src/main/java/com/tommytony/war/mapper/StructureVolumeMapper.java
@@ -6,10 +6,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import com.tommytony.war.War;
 import com.tommytony.war.structure.StructureType;
 import com.tommytony.war.volume.Volume;
+
+import org.bukkit.block.Block;
+import org.bukkit.util.Vector;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
@@ -22,7 +26,7 @@ import org.json.simple.JSONValue;
 
 public class StructureVolumeMapper {
 
-	public static int load(Volume volume, String name, StructureType type) {
+	public static StructureReturnType load(Volume volume, String name, StructureType type) {
 		int count = 0;
 		BufferedReader in = null;
 		Object o = null;
@@ -40,40 +44,62 @@ public class StructureVolumeMapper {
 		int xSize = (Integer) obj.get("xSize");
 		int ySize = (Integer) obj.get("ySize");
 		int zSize = (Integer) obj.get("zSize");
+		Vector specOffset = null;
 		int[][][] types = new int[xSize][ySize][zSize];
 		byte[][][] datas = new byte[xSize][ySize][zSize];
-		JSONArray array = (JSONArray) obj.get("blockdata");
+		JSONArray blocktype = (JSONArray) obj.get("blocktype");
+		JSONArray blockdata = (JSONArray) obj.get("blockdata");
+		int loopType; byte loopData;
 		for(int x = 0; x < xSize; x++) {
 			for(int y = 0; y < ySize; y++) {
 				for(int z = 0; z < zSize; z++) {
-					types[x][y][z] = (Integer) array.get(2 * x * y * z);
-					datas[x][y][z] = (Byte) array.get(2 * x * y * z + 1);
+					loopType = (Integer) blocktype.get((x * ySize * zSize) + (y * zSize) + (z));
+					loopData = (Byte) blockdata.get((x * ySize * zSize) + (y * zSize) + (z));
+					//special location checking
+					if(loopType == 0 && loopData == Byte.MAX_VALUE) {
+						//SPECIAL LOCATION :D
+						specOffset = new Vector(x, y, z);
+					}
 				}
 			}
 		}
+		count = xSize * ySize * zSize;
 		volume.setBlockTypes(types);
 		volume.setBlockDatas(datas);
-		return count;
+		
+		return new StructureVolumeMapper.StructureReturnType(count, specOffset);
 	}
 	
-	public static int save(Volume volume, String name, StructureType type) {
+	static protected class StructureReturnType {
+		public final int count;
+		public final Vector vec;
+		
+		StructureReturnType(int count, Vector vec) {
+			this.count = count;
+			this.vec = vec;
+		}
+	}
+	
+	public static int save(Volume volume, String name, StructureType type, String specLoc) {
 		int count = 0;
 		JSONObject obj = new JSONObject();
 		obj.put("name", name);
 		obj.put("xSize", new Integer(volume.getSizeX()));
 		obj.put("ySize", new Integer(volume.getSizeY()));
 		obj.put("zSize", new Integer(volume.getSizeZ()));
-		JSONArray blocks = new JSONArray();
+		JSONArray types = new JSONArray();
+		JSONArray datas = new JSONArray();
 		for(int x = 0; x < volume.getSizeX(); x++) {
 			for(int y = 0; y < volume.getSizeY(); y++) {
 				for(int z = 0; z < volume.getSizeZ(); z++) {
-					blocks.add(new Integer(volume.getBlockTypes()[x][y][z]));
-					blocks.add(new Integer(volume.getBlockDatas()[x][y][z]));
-					count += 2;
+					types.add(new Integer(volume.getBlockTypes()[x][y][z]));
+					datas.add(new Integer(volume.getBlockDatas()[x][y][z]));
+					count++;
 				}
 			}
 		}
-		obj.put("blockdata", blocks);
+		obj.put("blocktype", types);
+		obj.put("blockdata", datas);
 		BufferedWriter out = null;
 		try {
 			(new File(War.war.getDataFolder().getPath() + "/dat/structure/" + type.getString())).mkdir();
